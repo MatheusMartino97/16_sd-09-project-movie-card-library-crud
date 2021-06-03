b6272d675abf565c811b721fd3c476d6785798e3
Matheus Martino <matheus.martino97@gmail.com>
2021-06-03 08:43:39 -0300

    Updates

---
+++ precache-manifest.9056ce1413ba28542da4e5ca8d083155.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2986bdd0f3fb226a5aab27400055f937",
+    "url": "/16_sd-09-project-movie-card-library-crud/index.html"
+  },
+  {
+    "revision": "1ea020f671ae8e9b24c9",
+    "url": "/16_sd-09-project-movie-card-library-crud/static/css/main.5c65cfd2.chunk.css"
+  },
+  {
+    "revision": "e1bbad2829392aff160a",
+    "url": "/16_sd-09-project-movie-card-library-crud/static/js/2.91d36ce2.chunk.js"
+  },
+  {
+    "revision": "1ea020f671ae8e9b24c9",
+    "url": "/16_sd-09-project-movie-card-library-crud/static/js/main.0474de77.chunk.js"
+  },
+  {
+    "revision": "b8ae255c115114f4891c",
+    "url": "/16_sd-09-project-movie-card-library-crud/static/js/runtime-main.428ca23b.js"
+  }
+]);
